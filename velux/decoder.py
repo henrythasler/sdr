7fa361c7fe7a1f434c0306240c4398b539878904
Henry <git@thasler.org>
2024-05-05 13:09:14 +0200

    complete refactor

--- velux/decoder.py
+++ velux/decoder.py
@@ -10,7 +10,7 @@ pip install crc bitarray
 import numpy as np
 # from bitarray import bitarray
 from crc import Calculator, Crc16
-from bitstring import Bits, BitArray
+import bitstring
 
 """
 terminology according to https://dev.to/stungnet/from-data-to-frame-the-evolution-of-pdus-across-the-osi-model-21gd
@@ -24,10 +24,10 @@ terminology according to https://dev.to/stungnet/from-data-to-frame-the-evolutio
 f6 00 00 00 3f da 2c 93 00 01 61 d2 00 00 00 04 76 23 14 50 08 11 50 c8 97
 Dest ID   : 3f000000     Msg type  : f6            Message   : 0161d2000000  Counter   : 1142          MAC       : 231450081150
 """
-bits = Bits([0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1])
+bits = bitstring.Bits([0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1])
 
 # need to find the frame within raw input bits
-frame_start = bits.find(Bits(bin="0 11111111 1 0 11001100 1".replace(" ", ""))) # [0xff, 0x33] plus start/stop bits
+frame_start = bits.find(bitstring.Bits(bin="0 11111111 1 0 11001100 1".replace(" ", ""))) # [0xff, 0x33] plus start/stop bits
 
 if not frame_start:
     exit()
@@ -35,43 +35,39 @@ if not frame_start:
 frame_start = frame_start[0]
 
 print("Received {} bits. Frame begins at bit {}".format(len(bits), frame_start))
-frame = BitArray(bits[frame_start:])
-print(frame)
+frame = bitstring.BitArray(bits[frame_start:], )
 
 #remove start bits
 del frame[0:len(frame):10]
 #remove stop bits
 del frame[8:len(frame):9]
 
-# LSB is sent first, so we need to reverse it here
-# frame.bytereverse()
+# LSB is sent first for each byte, so we need to reverse it here
+for i in range(0, len(frame)-8, 8):
+    frame.reverse(i, i + 8)
 
-# deserialize bits into actual bytes
-# packet = frame.tobytes()
-# print(packet)
-
-# print("\r\nPacket: {}".format(packet.hex(' ')))
+# by now it's a packet
+packet = frame
+print("\r\nPacket: {}".format(packet.tobytes().hex(' ')))
 
 # separate message from meta-information
-options1, _ = frame.unpack("pad:16, uint:8, bin")
+_, options1, _ = packet.unpack("uint:16, uint:8, bin")
 payload_length = options1 & 0x1f
-print(payload_length)
-# crc16 = unpack('<H', packet[3 + payload_length: 3 + payload_length + 2])[0]
-# crc_calc = Calculator(Crc16.KERMIT).checksum(packet[2:3 + payload_length])
-"""
-payload = packet[3:3 + payload_length]
+
+_, _, crc16 = packet.unpack("bytes:3, bytes:{}, uintle:16".format(payload_length))
+crc_calc = Calculator(Crc16.KERMIT).checksum(packet.tobytes()[2:3 + payload_length])
+payload = packet[3*8:3*8 + payload_length*8]
 print("    Payload Length: {} bytes (0x{:02X}, {:08b}b), {} bits)".format(payload_length, payload_length, payload_length, payload_length * 8))
-print("    CRC16: {} (0x{:04X}, {:08b}b)".format("OK" if crc_calc == crc16 else "ERROR, expected {}".format(crc_calc), crc16, crc16))
+print("    CRC16: {} (0x{:04X}, {:08b}b)".format("OK" if crc_calc == crc16 else "ERROR, expected 0x{:04X} but received".format(crc_calc), crc16, crc16))
 
-print("\r\nMessage: {}".format(payload.hex(' ')))
+print("\r\nMessage: {}".format(payload.tobytes().hex(' ')))
 # deserialize message-content
-destination, source, payload, counter, mac1, mac2 = unpack('<II{}sHHI'.format(payload_length-16),payload)
-mac = (mac1 << 32) + mac2
+options2, destination, source, command, payload, counter, mac = payload.unpack('uint:8, uint:24, uint:24, uint:8, bytes:{}, uint:16, uint:48'.format(payload_length-16))
 
-print("    Source-Address: 0x{:02X}".format(source))
-print("    Target-Address: 0x{:02X}".format(destination))
+print("    Options2: 0x{:02X} (0b{:08b})".format(options2, options2))
+print("    Source-Address: 0x{:06X}".format(source))
+print("    Target-Address: 0x{:06X}".format(destination))
+print("    Command: 0x{:02X}".format(command))
 print("    Payload: {}".format(payload.hex(' ')))
-print("    Counter: 0x{:02X} ({})".format(counter, counter))
-print("    MAC: 0x{:06X}".format(mac))
-
-"""
+print("    Counter: 0x{:04X} ({})".format(counter, counter))
+print("    MAC: 0x{:012X}".format(mac))
